b2be5ed4100b1b97c48d3db9b39e115d5126e448
Austin Moen <moenaustin20@gmail.com>
2020-03-18 15:50:49 -0500

    initial commit

---
+++ BattleShip/Boards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleShip
+{
+    public class Boards
+    {
+        public int[,] board;
+        public void MakeBoard()
+        {
+            board = new int[20, 20];
+            int rowLength = board.GetLength(0);
+            int colHeight = board.GetLength(1);
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colHeight; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BattleShip/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BattleShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Boards boards = new Boards();
+            boards.MakeBoard();
+        }
+    }
+}
